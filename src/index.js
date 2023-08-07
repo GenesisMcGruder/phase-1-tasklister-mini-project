6d2229615cdb3391347685f40e7bbc295d83e41e
Genesis McGruder <genesismcgruder@gmail.com>
2023-08-07 03:35:10 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+ form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createNewDiv(e.target.new_task_description.value)
+    form.reset()
+  })
+})
+
+ function createNewDiv(des) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${des} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+ }
+
+
+ function handleDelete(e) {
+  e.target.parentNode.remove()
+ }
